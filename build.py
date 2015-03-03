8c592c2e9defd624f60b9d250a76d1e5e0d006a0
Fabio Scala <fabio.scala@gmail.com>
2015-03-03 13:53:28 +0100

    DAT-44 fix travis

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ from pybuilder.core import use_plugin, init
 
 use_plugin('python.core')
 use_plugin('python.unittest')
+use_plugin('python.coverage')
 use_plugin('python.flake8')
 use_plugin('python.install_dependencies')
 use_plugin('pypi:pybuilder_django_enhanced_plugin')
