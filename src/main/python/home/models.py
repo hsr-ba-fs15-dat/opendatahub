549fe8b15160c42e7ea2a845a8428373a41250aa
Christoph Hüsler <christoph.huesler@burnstone.ch>
2015-03-03 14:30:05 +0100

    pipeline + models, PoC

--- src/main/python/home/models.py
+++ src/main/python/home/models.py
@@ -1,3 +1,63 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+
+
+class Node(object):
+    def __init__(self, name=None, parameters=None):
+        """
+        :type name: str
+        :type parameters: dict
+        """
+        self.name = name or self.__class__.__name__
+        self.parameters = parameters or {}
+
+
+class Input(Node):
+    def __init__(self, name=None, parameters=None):
+        super(Input, self).__init__(name, parameters)
+
+    def read(self):
+        pass
+
+
+class Output(Node):
+    def __init__(self, name=None, parameters=None):
+        super(Output, self).__init__(name, parameters)
+
+    def write(self, reader):
+        pass
+
+
+class Transformation(Node):
+    def __init__(self, name=None, parameters=None):
+        super(Transformation, self).__init__(name, parameters)
+
+    def transform(self, reader):
+        pass
+
+
+class Merge(Node):
+    def __init__(self, name=None, parameters=None):
+        super(Merge, self).__init__(name, parameters)
+
+    def merge(self, readers):
+        pass
+
+
+class Pipeline(object):
+    def __init__(self, name, description, nodes):
+        self.name = name
+        self.description = description
+        self.nodes = nodes
+
+    def run(self):
+        prev = None
+
+        for node in self.nodes:
+            if isinstance(node, Input):
+                prev = node.read()
+            elif isinstance(node, Transformation):
+                prev = node.transform(prev)
+            elif isinstance(node, Output):
+                node.write(prev)
---
+++ src/test/python/transformations/transformations.py
@@ -0,0 +1,66 @@
+__author__ = 'chuesler'
+
+import home.models
+import os
+import csv
+
+
+class FileInput(home.models.Input):
+    def __init__(self, filename):
+        super(FileInput, self).__init__(parameters={'filename': filename})
+
+    def read(self):
+        with open(self.parameters['filename'], 'r') as file_input:
+            for line in file_input:
+                yield line
+
+
+class CsvInput(home.models.Transformation):
+    def __init__(self):
+        super(CsvInput, self).__init__()
+
+    def transform(self, reader):
+        csv_reader = csv.DictReader(reader)
+        for row in csv_reader:
+            yield row
+
+
+class NormalizedAddressToGarbageTransform(home.models.Transformation):
+    def __init__(self):
+        super(NormalizedAddressToGarbageTransform, self).__init__()
+
+    def transform(self, reader):
+        for row in reader:
+            converted = {
+                'Name': '{} {}'.format(row['Name'], row['Surname']),
+                'Street': '{} {}'.format(row['Street'], row['No']),
+                'City': '{} {}'.format(row['Zip'], row['City'])
+            }
+            yield converted
+
+
+class CsvOutput(home.models.Output):
+    def __init__(self, filename, field_names):
+        super(CsvOutput, self).__init__(parameters={'filename': filename, 'field_names': field_names})
+
+    def write(self, reader):
+        with open(self.parameters['filename'], 'w') as output:
+            csvWriter = csv.DictWriter(output, self.parameters['field_names'])
+            for row in reader:
+                csvWriter.writerow(row)
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+fileIn = FileInput(os.path.join(basedir, 'test-addresses.csv'))
+csvIn = CsvInput()
+transform = NormalizedAddressToGarbageTransform()
+csvOut = CsvOutput(os.path.join(basedir, 'test-addresses-output.csv'), ('Name', 'Street', 'City'))
+
+p = home.models.Pipeline('TestPipeline', 'Example pipeline to test the api', [fileIn, csvIn, transform, csvOut])
+p.run()
+
+# Anrede, Vorname, Name, Strasse, Hausnummer, PLZ, Ort
+# Name, Strasse, Ort
+
+
