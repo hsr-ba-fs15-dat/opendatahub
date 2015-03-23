e46259ae07a3c9f5a68467bf8f8280860498c984
Fabio Scala <fabio.scala@gmail.com>
2015-03-23 11:53:28 +0100

    added formatter infrastructure. gdal deps for travis. pylint/flake8 cleanups

--- src/main/python/hub/tests/simple_pipeline.py
+++ src/main/python/hub/tests/simple_pipeline.py
@@ -1,10 +1,10 @@
-import hub.nodes
-
-
-h = hub.nodes.HttpInput('http://localhost:5000/sample/sample.csv')
-c = hub.nodes.CsvInput()
-d = hub.nodes.DatabaseWriter(desc='Random garbage', name='Test')
-
-http_input = h.read()
-csv_input = c.parse(http_input)
-d.write(csv_input)
+# import hub.nodes
+#
+#
+# h = hub.nodes.HttpInput('http://localhost:5000/sample/sample.csv')
+# c = hub.nodes.CsvInput()
+# d = hub.nodes.DatabaseWriter(desc='Random garbage', name='Test')
+#
+# http_input = h.read()
+# csv_input = c.parse(http_input)
+# d.write(csv_input)
