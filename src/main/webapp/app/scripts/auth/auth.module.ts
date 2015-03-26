5c1eed5c1d630a39ae563b6d71339bba63218cb5
Remo Liebi <rliebi@hsr.ch>
2015-03-26 16:33:30 +0100

    DAT-50 assigned Heroku ID

--- src/main/webapp/app/scripts/auth/auth.module.ts
+++ src/main/webapp/app/scripts/auth/auth.module.ts
@@ -17,13 +17,13 @@ module odh.auth {
     function config($stateProvider:ng.ui.IStateProvider, $authProvider) {
         $authProvider.facebook({
             url: '/api/v1/auth/social/',
-            clientId: '401522313351953' // this is the local id
-            // clientId: '401520096685508' // this is the heroku id
+            // clientId: '401522313351953' // this is the local id
+            clientId: '401520096685508' // this is the heroku id
         });
         $authProvider.github({
             url: '/api/v1/auth/social/',
-            clientId: 'f29d882c342818c82e0b' // this is the local id
-            // clientId: '8ef558ed3fb0f5385da5' // this is the heroku id
+            // clientId: 'f29d882c342818c82e0b' // this is the local id
+            clientId: '8ef558ed3fb0f5385da5' // this is the heroku id
             // clientId: 'b24753ec88ca98150354' // localhost id
         });
         $stateProvider
