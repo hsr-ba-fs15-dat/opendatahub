c7d4057b06afeb15818b26be4f45e962170ca314
Remo Liebi <rliebi@hsr.ch>
2015-03-30 16:17:54 +0200

    Fixed Gunicorn Settings for Heroku

--- runserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"/src/main/python
-python ./manage.py runserver_plus 0.0.0.0:5000
--- src/main/python/opendatahub/wsgi.py
+++ src/main/python/opendatahub/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opendatahub.settings")
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
