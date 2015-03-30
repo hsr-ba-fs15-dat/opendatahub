d7221cadda7da782fc32f5a5a79d278839769d46
Fabio Scala <fabio.scala@gmail.com>
2015-03-30 18:12:53 +0200

    change webapp/static dir to absolute path

--- src/main/python/opendatahub/urls.py
+++ src/main/python/opendatahub/urls.py
@@ -1,8 +1,9 @@
+import os
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
 
-from opendatahub.settings import PRODUCTION, STATIC_ROOT
+from opendatahub.settings import PRODUCTION, STATIC_ROOT, BASE_DIR
 from hub.views.document import DocumentViewSet
 from hub.views.file_group import FileGroupViewSet
 from hub.views.file import FileViewSet
@@ -21,18 +22,24 @@ urlpatterns = (
     url(r'api/v1/auth/', include('authentication.urls')),
 )
 
+WEBAPP_DIR = os.path.join(os.path.dirname(BASE_DIR), 'webapp')
+
 if PRODUCTION:
+    WEBAPP_DIR = os.path.join(WEBAPP_DIR, 'dist')
+
     urlpatterns += (
-        url(r'^$', 'django.views.static.serve', {'path': 'index.html', 'document_root': 'src/main/webapp/dist'}),
+        url(r'^$', 'django.views.static.serve', {'path': 'index.html', 'document_root': WEBAPP_DIR}),
         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT}),
-        url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'src/main/webapp/dist'}),
+        url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': WEBAPP_DIR}),
     )
 else:
+    WEBAPP_DIR = os.path.join(WEBAPP_DIR, 'app')
+
     urlpatterns += (
-        url(r'^$', 'django.views.static.serve', {'path': 'index.html', 'document_root': '../webapp/app'}),
+        url(r'^$', 'django.views.static.serve', {'path': 'index.html', 'document_root': WEBAPP_DIR}),
         url(r'^bower_components/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': '../webapp/bower_components'}),
-        url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': '../webapp/app'}),
+        url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': WEBAPP_DIR}),
     )
 
 urlpatterns = patterns('', *urlpatterns)
