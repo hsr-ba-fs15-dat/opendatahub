7b55e6467d4278bba9683c51c63885425b0aa846
Remo Liebi <rliebi@hsr.ch>
2015-04-30 01:19:36 +0200

    DAT-116 #time 1h imporved tablechoser

--- src/main/webapp/app/scripts/main/directives/transformation.choser.ts
+++ src/main/webapp/app/scripts/main/directives/transformation.choser.ts
@@ -4,20 +4,70 @@
 module odh.main {
     'use strict';
 
-    function odhChooseTransformation():ng.IDirective {
-        return {
+    export class odhChooseTransformation implements ng.IDirective {
+        static $inject = ['private DocumentService:main.DocumentService', 'ngTableParams:any',
+            'FileGroupService:main.FileGroupService', '$auth:any', 'ToastService:odh.utils.ToastService'];
 
-            restrict: 'AE',
-            templateUrl: 'views/transformation.table_choose.html',
-            scope: {
-                tableParams: '=',
-                isAuthenticated: '&',
-                addRemoveTable: '&',
-                checkTableSelected: '&',
-                getFileGroup: '&'
-            }
+        constructor(private DocumentService:main.DocumentService, private ngTableParams:any, private $auth:any,
+                    private FileGroupService:main.FileGroupService, private ToastService:odh.utils.ToastService) {
+        }
+
+
+        restrict = 'AE';
+        templateUrl = 'views/transformation.table_choose.html';
+        scope = {
+            addRemoveTable: '&',
+            checkTableSelected: '&'
         };
+        link = (scope) => {
+            scope.tableParams = new this.ngTableParams({
+                page: 1,            // show first page
+                count: 10           // count per page
+            }, {
+                counts: [10, 25, 50, 100],
+                total: 0, // length of data
+                getData: ($defer, params) => {
+                    this.DocumentService.getList(params.url()).then(result => {
+                        params.total(result.count);
+                        $defer.resolve(result.results);
+                    });
+                }
+            });
+            scope.isAuthenticated = () => {
+                return this.$auth.isAuthenticated();
+            };
+
+            scope.getFileGroup = (document, count = 3) => {
+                this.FileGroupService.getAll(document.id, true, count).then(filegroups => {
+                    if (!document.$showRows) {
+                        angular.forEach(filegroups, (fg) => {
+                            angular.forEach(fg.preview, (preview) => {
+                                preview.uniqueId = preview.unique_name;
+                                preview.cols = [];
+                                preview.parent = fg.id;
+                                preview.private = document.private;
+                                angular.forEach(preview.columns, (col) => {
+                                    preview.cols.push({name: col, alias: col, type: preview.types[col]});
+                                });
+                            });
+                        });
+                        document.fileGroup = filegroups;
+                    } else {
+                        document.fileGroup = [];
+                    }
+                    document.$showRows = !document.$showRows;
+                }).catch(error => this.ToastService.failure('Es ist ein Fehler aufgetreten.'));
+            }
+
+
+        };
+
+
     }
 
-    angular.module('openDataHub.main').directive('odhChooseTransformation', odhChooseTransformation);
+    angular.module('openDataHub.main').directive('odhChooseTransformation',
+        (DocumentService:main.DocumentService, ngTableParams:any, $auth:any, FileGroupService:main.FileGroupService,
+         ToastService:odh.utils.ToastService) => {
+            return new odhChooseTransformation(DocumentService, ngTableParams, $auth, FileGroupService, ToastService);
+        });
 }
