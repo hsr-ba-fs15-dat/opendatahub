252432cdae1bc9795305c6287bb2c799b44f2209
Remo Liebi <rliebi@hsr.ch>
2015-04-29 15:37:12 +0200

    DAT-116 #time 3h added Settings for Prefixes, in detail view checked for possible Own Table,  added String.format()

--- src/main/webapp/app/scripts/main/controllers/transformation.detail.controller.ts
+++ src/main/webapp/app/scripts/main/controllers/transformation.detail.controller.ts
@@ -1,8 +1,8 @@
 /// <reference path='../../all.d.ts' />
 
-
 module odh.main {
     'use strict';
+
     enum transType {Template, Final}
     class TransformationDetailController implements main.ITransformation {
 
@@ -14,24 +14,28 @@ module odh.main {
         public 'private';
         public columns;
         public rows;
-        public alerts;
-        public fileGroupTable;
+        public alerts:any;
+        public fileGroupTables:ITable[] = [];
         public usedTables:{};
         public previewError;
         public selected;
         public transformationType:transType = transType.Template;
-
         public allowDelete:boolean;
+        public transformationPrefix:string;
+        public packagePrefix:string;
 
         constructor(private $stateParams:any,
                     private TransformationService:main.TransformationService,
                     private $state:ng.ui.IStateService,
                     private ToastService:odh.utils.ToastService,
-                    private ngTableParams,
                     private $auth:any,
-                    public $filter:ng.IFilterService,
-                    private $modal:ng.ui.bootstrap.IModalService) {
+                    private $modal:ng.ui.bootstrap.IModalService, private AppConfig:odh.IAppConfig) {
             // controller init
+            AppConfig.then(config => {
+                this.transformationPrefix = config.TRANSFORMATION_PREFIX;
+                this.packagePrefix = config.PACKAGE_PREFIX;
+            });
+            this.fileGroupTables = [];
             this.transformationId = $stateParams.id;
             this.TransformationService.get(this.transformationId).then(data => {
                 this.name = data.name;
@@ -45,26 +49,28 @@ module odh.main {
                 this.selected = {};
 
             });
-            this.fileGroupTable = new ngTableParams({
-                    page: 1,
-                    count: 10
-                }, {
-                    counts: [],
-                    total: 0,
-                    getData: ($defer, params) => {
-                        this.TransformationService.get(this.transformationId).then(result => {
-                            this.selected.vals = result.file_groups;
-                            console.log(this.selected);
-                            params.total(result.file_groups.length);
-                            $defer.resolve(result.file_groups);
-                        });
 
-                    }
-                }
-            );
 
         }
 
+        /**
+         * Checks if this Table could belongs to our system.
+         */
+
+        public checkIfOurTable(table:main.ITable) {
+            var rxQry = '^({0}|{1})\\d+_.*$'.format(this.packagePrefix, this.transformationPrefix);
+            var regEx = new RegExp(rxQry);
+            return regEx.test(table.name);
+        }
+
+        public checkIfTableInDB(table:main.ITable) {
+            if (this.checkIfOurTable(table)) {
+                // todo: check if table name exists in database
+
+
+            }
+            return this.checkIfOurTable(table);
+        }
 
         public aceLoaded(editor) {
             editor.$blockScrolling = 'Infinity';
@@ -85,7 +91,6 @@ module odh.main {
                 this.rows = null;
                 this.previewError = 'Diese Transformation enthält (noch) keine gültigen Daten';
                 this.ToastService.failure('Diese Transformation enthält keine gültigen Daten');
-                this.alerts.push({msg: data.error, type: 'danger', title: 'Fehler:'});
             });
         }
 
@@ -106,7 +111,7 @@ module odh.main {
     }
 
     class DeleteTransformationController {
-        constructor(private $modalInstance: ng.ui.bootstrap.IModalServiceInstance) {
+        constructor(private $modalInstance:ng.ui.bootstrap.IModalServiceInstance) {
         }
 
         public ok() {
