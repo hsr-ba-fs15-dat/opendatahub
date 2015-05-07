afe4d81fdd788cd42df69a22c532b6afdb9cb716
Christoph Hüsler <christoph.huesler@burnstone.ch>
2015-05-07 11:37:32 +0200

    fix permissions for filegroups, urls

--- src/main/python/authentication/permissions.py
+++ src/main/python/authentication/permissions.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-from hub.models import DocumentModel, FileGroupModel, FileModel, TransformationModel
+from hub.models import DocumentModel, FileGroupModel, FileModel, TransformationModel, UrlModel
 
 
 class IsOwnerOrReadOnly(BasePermission):
@@ -11,28 +11,24 @@ class IsOwnerOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
 
-        return obj.owner == request.user
+        return hasattr(obj, 'owner') and obj.owner == request.user
 
 
 class IsOwnerOrPublic(BasePermission):
-
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            if isinstance(obj, DocumentModel):
-                return self._check_document_permission(obj, request.user)
-            elif isinstance(obj, FileGroupModel):
-                return self._check_document_permission(obj.document, request.user)
-            elif isinstance(obj, FileModel):
-                return self._check_document_permission(obj.file_group.document, request.user)
-            elif isinstance(obj, TransformationModel):
-                return self._check_document_permission(obj, request.user)
-            else:
-                return False
-        else:
-            if obj.owner == request.user:
-                return True
+        safe = request.method in SAFE_METHODS
 
-        return request.user is not None
+        if isinstance(obj, DocumentModel):
+            return self._check_document_permission(safe, obj, request.user)
+        elif isinstance(obj, FileGroupModel):
+            return self._check_document_permission(safe, obj.document, request.user)
+        elif isinstance(obj, (FileModel, UrlModel)):
+            return self._check_document_permission(safe, obj.file_group.document, request.user)
+        elif isinstance(obj, TransformationModel):
+            return self._check_document_permission(safe, obj, request.user)
 
-    def _check_document_permission(self, doc, user):
-        return not doc.private or (user and doc.owner == user)
+        return False
+
+    def _check_document_permission(self, safe, doc, user):
+        own = user and doc.owner == user
+        return (not safe and own) or (safe and (own or not doc.private))
