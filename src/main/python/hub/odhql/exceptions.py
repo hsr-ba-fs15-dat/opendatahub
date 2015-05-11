22010648dcc151d048431b91b3b11afd48e5e03a
Christoph Hüsler <christoph.huesler@burnstone.ch>
2015-05-11 19:22:08 +0200

    pylint says odhql/parser.py is too large - split it up

--- src/main/python/hub/odhql/exceptions.py
+++ src/main/python/hub/odhql/exceptions.py
@@ -7,3 +7,8 @@ from __future__ import unicode_literals
 
 class OdhQLExecutionException(Exception):
     pass
+
+
+class TokenException(Exception):
+    def __init__(self, message='parse error'):
+        super(TokenException, self).__init__(message)
