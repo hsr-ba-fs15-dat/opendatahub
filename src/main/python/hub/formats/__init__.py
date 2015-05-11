d9d7f59a958a24f79416bc0c3cffd3caf386bf5e
Christoph Hüsler <christoph.huesler@burnstone.ch>
2015-05-11 10:36:41 +0200

    interlis 1 model generator as formatter

--- src/main/python/hub/formats/__init__.py
+++ src/main/python/hub/formats/__init__.py
@@ -5,4 +5,6 @@
 from .base import Format, CSV, JSON, Excel, GML, GeoJSON  # noqa
 from .base import KML, Shapefile, XML, INTERLIS1, INTERLIS2, WFS, GeoCSV, Other  # noqa
 
+from .interlis_model import InterlisModelFormat  # noqa
+
 identify = Format.identify
--- src/main/python/hub/formatters/interlis_model.py
+++ src/main/python/hub/formatters/interlis_model.py
@@ -1,32 +1,23 @@
 from django.utils.text import slugify
 
-from hub.models import TransformationModel, DocumentModel
-from hub.utils.odhql import TransformationUtil
+import math
+
 from hub.structures.frame import OdhType
+from .base import Formatter
+from hub.formats import InterlisModelFormat
 
 
-class InterlisOneUtil(object):
-    def generate_transfer_model(self, obj):
-        return self.get_model(obj).get_model_definition()
+class InterlisModelFormatter(Formatter):
+    targets = InterlisModelFormat
 
-    def get_model(self, obj):
-        if isinstance(obj, TransformationModel):
-            table = Table(obj.name, TransformationUtil.df_for_transformation(obj))
-            topic = Topic(obj.name, [table])
-            return Model(obj.name, [topic])
-        elif isinstance(obj, DocumentModel):
-            topics = []
-            for fg in obj.groups.all():
-                tables = []
+    @classmethod
+    def format(cls, dfs, name, format, *args, **kwargs):
+        tables = []
+        for df in dfs:
+            tables.append(Table(df.name, df))
+        model = Model(name, Topic(name, tables))
 
-                file_group = fg.to_file_group()
-                dfs = file_group.to_df()
-                for df in dfs:
-                    tables.append(Table(df.name, df))
-                topics.append(Topic(file_group.get_main_file().name, tables))
-            return Model(obj.name, topics)
-
-        raise RuntimeError('unknown model type: {}'.format(type(obj)))
+        return model.get_model_definition()
 
 
 class Model(object):
@@ -35,7 +26,10 @@ class Model(object):
         self.topics = topics
 
     def get_model_definition(self):
-        result = "TRANSFER {}; \n\nDOMAIN".format(self.name)
+        result = 'TRANSFER {}; \n\nDOMAIN'.format(self.name)
+        result += '!! ACHTUNG: Dies ist ein automatisch generiertes Modell und sollte nicht ohne Anpassungen'
+        result += '!! verwendet werden.'
+
         domain = {}
         for topic in self.topics:
             for table in topic.tables:
@@ -87,6 +81,12 @@ class Table(object):
         result += "\tEND {}.\n".format(self.name)
         return result
 
+    def next_nines(self, x):
+        '''
+        results in the next series of 999...
+        '''
+        return int(10 ** (math.floor(math.log10(x) + 1)) - 1)
+
     def get_fields(self):
         domain = {}
         fields = []
@@ -98,18 +98,19 @@ class Table(object):
 
                 fields.append((name, "TEXT*{}".format(max_length)))
             elif type in (OdhType.INTEGER, OdhType.BIGINT, OdhType.SMALLINT):
-                import math
-
-                # results in the next series of 999...
-                next_nines = lambda x: int(10 ** (math.floor(math.log10(x) + 1)) - 1)
 
                 min = self.df[name].min()
-                min = -next_nines(-min) if min and min < 0 else 0
+                min = -self.next_nines(-min) if min and min < 0 else 0
 
                 max = self.df[name].max()
-                max = next_nines(max) if max and max > 0 else 0
+                max = self.next_nines(max) if max and max > 0 else 0
 
                 fields.append((name, "[{} .. {}]".format(min, max)))
+            elif type == OdhType.FLOAT:
+                max = self.df[name].max()
+                max = self.next_nines(max) if max and max > 0 else 0
+
+                fields.append((name, '[0.000 .. {}.999]'.format(max)))
             elif type == OdhType.DATETIME:
                 fields.append((name, "DATE"))  # actually, this can't include time in interlis. oh well.
             else:
@@ -121,23 +122,14 @@ class Table(object):
 
                     if isinstance(value, shp.Point):
                         fields.append((name, 'POINT'))
-                        # FIXME this needs srid, right?
                         domain['POINT'] = 'COORD2 480000.000 70000.000 850000.000 310000.000'
                     elif isinstance(value, (shp.LineString, shp.LinearRing)):
                         fields.append((name, 'POLYLINE WITH (\'STRAIGHTS\') '
                                              'VERTEX COORD2 480000.000 70000.000 850000.000 310000.000'))
                     elif isinstance(value, shp.Polygon):
-                        fields.appen((name, 'AREA WITH (\'STRAIGHTS\') '
-                                            'VERTEX COORD2 480000.000 70000.000 850000.000 310000.000'))
+                        fields.append((name, 'AREA WITH (\'STRAIGHTS\') '
+                                             'VERTEX COORD2 480000.000 70000.000 850000.000 310000.000'))
                     else:
                         raise RuntimeError('Unable to generate interlis model for data type {}'.format(type(value)))
 
         return fields, domain
-
-
-import django
-
-django.setup()
-
-model = TransformationModel.objects.get(id=20)
-print InterlisOneUtil().generate_transfer_model(model)
