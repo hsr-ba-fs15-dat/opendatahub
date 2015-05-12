a3832e9eb60dce806a5fe8f047f044ad18831eb5
Christoph Hüsler <christoph.huesler@burnstone.ch>
2015-05-12 17:45:11 +0200

    fix interlis model generator
    compiler doesn't complain any more, geometries may still be wrong though

--- src/main/python/hub/formatters/interlis_model.py
+++ src/main/python/hub/formatters/interlis_model.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 import math
 
-from django.utils.text import slugify
+import re
 
 from hub.structures.frame import OdhType
 from hub.structures.file import File
@@ -26,7 +26,7 @@ class InterlisModelFormatter(Formatter):
 
 class Model(object):
     def __init__(self, name, topics):
-        self.name = slugify(unicode(name))
+        self.name = sanitize_name(name)
         self.topics = topics
 
     def get_model_definition(self):
@@ -34,57 +34,60 @@ class Model(object):
         result += '!! ACHTUNG: Dies ist ein automatisch generiertes Modell und sollte nicht ohne Anpassungen \n'
         result += '!! verwendet werden.\n\n'
 
-        result += 'DOMAIN\n\n'
-
         domain = {}
         for topic in self.topics:
             for table in topic.tables:
                 domain.update(table.domain)
 
-        for k, v in domain.iteritems():
-            result += "\t{}: {};\n".format(k, v)
+        if len(domain) > 0:
+            result += 'DOMAIN\n\n'
 
-        result += "\n\nMODEL {}\n\n".format(self.name)
+            for k, v in domain.iteritems():
+                result += '\t{} = {};\n'.format(k, v)
+
+        result += '\nMODEL {}\n\n'.format(self.name)
 
         for topic in self.topics:
             result += topic.get_topic_definition()
 
-        result += "\nEND {}.\n\n".format(self.name)
+        result += '\nEND {}.\n\n'.format(self.name)
 
-        result += "\nCODE\n\tBLANK = DEFAULT, UNDEFINED = DEFAULT, CONTINUE = DEFAULT;\n\t TID = ANY;\n\nEND."
+        result += 'FORMAT FREE;\n\n'
+
+        result += '\nCODE\n\tBLANK = DEFAULT, UNDEFINED = DEFAULT, CONTINUE = DEFAULT;\n\t TID = ANY;\n\nEND.'
 
         return result
 
 
 class Topic(object):
     def __init__(self, name, tables):
-        self.name = slugify(unicode(name))
+        self.name = sanitize_name(name)
         self.tables = tables
 
     def get_topic_definition(self):
-        result = "TOPIC {}\n\n".format(self.name)
+        result = 'TOPIC {} = \n\n'.format(self.name)
 
         for table in self.tables:
             result += table.get_table_definition()
 
-        result += "\nEND {}.\n".format(self.name)
+        result += '\nEND {}.\n'.format(self.name)
         return result
 
 
 class Table(object):
     def __init__(self, name, df):
-        self.name = slugify(unicode(name))
+        self.name = sanitize_name(name)
         self.df = df
         self.fields, self.domain = self.get_fields()
 
     def get_table_definition(self):
-        result = "\tTABLE {} = \n".format(self.name)
+        result = '\tTABLE {} = \n'.format(self.name)
 
         for field in self.fields:
-            result += "\t\t{}: {};\n".format(slugify(unicode(field[0])), field[1])
+            result += '\t\t{}: {};\n'.format(sanitize_name(field[0]), field[1])
 
-        result += "\tNO IDENT\n"
-        result += "\tEND {}.\n".format(self.name)
+        result += '\tNO IDENT\n'
+        result += '\tEND {};\n'.format(self.name)
         return result
 
     def next_nines(self, x):
@@ -99,10 +102,12 @@ class Table(object):
 
         for name in self.df.columns:
             type = self.df[name].odh_type
-            if type == OdhType.TEXT:
-                max_length = self.df[name].str.len().max() if self.df[name].any() else 0
 
-                fields.append((name, "TEXT*{}".format(max_length)))
+            ili_type = '!! Unbekannter Typ'
+            if type == OdhType.TEXT:
+                max_length = self.df[name].str.len().max() if self.df[name].any() else 10
+
+                ili_type = 'TEXT*{}'.format(int(max_length))
             elif type in (OdhType.INTEGER, OdhType.BIGINT, OdhType.SMALLINT):
 
                 min = self.df[name].min()
@@ -111,14 +116,17 @@ class Table(object):
                 max = self.df[name].max()
                 max = self.next_nines(max) if max and max > 0 else 0
 
-                fields.append((name, "[{} .. {}]".format(min, max)))
+                ili_type = '[{} .. {}]'.format(min, max)
             elif type == OdhType.FLOAT:
                 max = self.df[name].max()
                 max = self.next_nines(max) if max and max > 0 else 0
 
-                fields.append((name, '[0.000 .. {}.999]'.format(max)))
+                ili_type = '[0.000 .. {}.999]'.format(max)
+            elif type == OdhType.BOOLEAN:
+                ili_type = 'BOOLEAN'
+                domain['BOOLEAN'] = '(True, False)'
             elif type == OdhType.DATETIME:
-                fields.append((name, "DATE"))  # actually, this can't include time in interlis. oh well.
+                ili_type = 'DATE'  # actually, this can't include time in interlis. oh well.
             else:
                 first_valid = self.df[name].first_valid_index()
                 if type == OdhType.GEOMETRY and first_valid is not None:
@@ -127,15 +135,25 @@ class Table(object):
                     value = self.df[name][first_valid]
 
                     if isinstance(value, shp.Point):
-                        fields.append((name, 'POINT'))
+                        ili_type = 'POINT'
                         domain['POINT'] = 'COORD2 480000.000 70000.000 850000.000 310000.000'
                     elif isinstance(value, (shp.LineString, shp.LinearRing)):
-                        fields.append((name, 'POLYLINE WITH (\'STRAIGHTS\') '
-                                             'VERTEX COORD2 480000.000 70000.000 850000.000 310000.000'))
+                        ili_type = ('POLYLINE WITH (STRAIGHTS) '
+                                    'VERTEX COORD2 480000.000 70000.000 850000.000 310000.000 '
+                                    'WITHOUT OVERLAPS > 0.001')
                     elif isinstance(value, shp.Polygon):
-                        fields.append((name, 'AREA WITH (\'STRAIGHTS\') '
-                                             'VERTEX COORD2 480000.000 70000.000 850000.000 310000.000'))
+                        ili_type = ('AREA WITH (STRAIGHTS) '
+                                    'VERTEX COORD2 480000.000 70000.000 850000.000 310000.000 '
+                                    'WITHOUT OVERLAPS > 0.001')
                     else:
-                        fields.append((name, '!! Geometrie-Feld'))
+                        ili_type = '!! Geometrie-Feld'
+
+            optional = 'OPTIONAL ' if self.df[name].isnull().any() else ''
+            fields.append((name, optional + ili_type))
 
         return fields, domain
+
+
+def sanitize_name(name):
+    sanitized = re.sub(r'[^A-Za-z0-9_\s]', '', name)
+    return ''.join([s.capitalize() for s in re.split(r'\s', sanitized.strip())])
