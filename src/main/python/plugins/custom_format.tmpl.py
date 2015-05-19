7bb817dacb71d121a0ad7ba5cf53108700243418
Fabio Scala <fabio.scala@gmail.com>
2015-05-19 11:23:42 +0200

    plugin examples

--- src/main/python/plugins/custom_format.tmpl.py
+++ src/main/python/plugins/custom_format.tmpl.py
@@ -1,2 +1,61 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import
+
+"""
+Example skeleton/template of a custom format implementation (parser and formatter)
+See hub.parsers package for concrete implementation examples.
+"""
+
+from hub.formats import Format, Formatter, Parser
+
+
+class MyFormat(Format):
+    label = 'MyFMT'
+
+    description = """
+    Kurze Beschreibung dieses Formats.
+    """
+
+    extension = 'myfmt'
+
+    @classmethod
+    def is_format(cls, file, *args, **kwargs):
+        return file.extension == cls.extension
+
+
+class MyFormatFormatter(Formatter):
+    targets = MyFormat,
+
+    @classmethod
+    def format(cls, dfs, name, format, *args, **kwargs):
+        """
+        :param dfs: List of DataFrames to format/convert
+        :param name: Deprecated. Basename for the target file(s). Use df.name instead.
+        :param format: Requested format. Useful if we can actually handle multiple formats.
+        :return: One or a list of FileGroup objects
+        """
+        file_groups = []
+
+        for df in dfs:
+            pass  # do the magic here and append to file_groups
+
+        return file_groups
+
+
+class MyFormatParser(Parser):
+    accepts = MyFormat,
+
+    @classmethod
+    def parse(cls, file, format, *args, **kwargs):
+        """
+        :param file: The file identified by the format
+        :param format: The format which matched (in case we accept multiple)
+        :return: A DataFrame (GeoDataFrame for Geo Data)
+        """
+        fg = file.file_group  # file group contains all files with the same basename
+        # do something with file.stream
+        # or with each file in fg
+        # parse data into one or multiple DataFrames
+
+        dfs = []
+        return dfs
--- src/main/python/plugins/odhql_function.tmpl.py
+++ src/main/python/plugins/odhql_function.tmpl.py
@@ -1,2 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+"""
+Example CONCAT function used in ODHQL. Copy this file and use a valid Python module name (e.g. my_function.py).
+Your function will then be automatically loaded and made available within the ODH Query Language Interpreter.
+See hub.odhql.function package for more concrete implementation examples.
+"""
+
+from hub.odhql.functions.core import VectorizedFunction
+
+
+class Concat(VectorizedFunction):
+    """
+    Konkateniert eine Liste von TEXT-Spalten oder Werten.
+
+    Parameter
+        - `args`: Liste von TEXT-Spalten oder -Werten
+
+    Beispiel
+        .. code:: sql
+
+            CONCAT(ODH5.given_name, ' ', ODH5.surname) AS name
+    """
+    name = 'CONCAT'
+
+    def apply(self, a, b, *args):
+        args = [self.expand(arg) for arg in [a, b] + list(args)]
+        for arg in args:
+            self.assert_str('string', arg)
+        return args[0].str.cat(args[1:])
