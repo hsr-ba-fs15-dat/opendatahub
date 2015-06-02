4653b1833c0af75fe2467c32d9d25533c2f7792c
Remo Liebi <rliebi@hsr.ch>
2015-06-02 14:40:07 +0200

    DAT-211 #time 3h fixed oauth facebook bug

--- src/main/python/authentication/config.py
+++ src/main/python/authentication/config.py
@@ -7,7 +7,6 @@ from __future__ import unicode_literals
 
 import os
 
-
 FACEBOOK_SECRET = os.environ.get('FACEBOOK_SECRET') or 'Facebook Client Secret'
 GITHUB_SECRET = os.environ.get('GITHUB_SECRET') or 'GitHub Client Secret'
 FOURSQUARE_SECRET = os.environ.get('FOURSQUARE_SECRET') or 'Foursquare Client Secret'
@@ -16,6 +15,9 @@ LINKEDIN_SECRET = os.environ.get('LINKEDIN_SECRET') or 'LinkedIn Client Secret'
 TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY') or 'Twitter Consumer Secret'
 TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET') or 'Twitter Consumer Secret'
 TWITTER_CALLBACK_URL = os.environ.get('TWITTER_CALLBACK_URL') or 'Twitter Redirect URI'
-
+SOCIAL_AUTH_FACEBOOK_SECRET = FACEBOOK_SECRET
+SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('FACEBOOK_PUBLIC', '401522313351953')
 FACEBOOK_PUBLIC = os.environ.get('FACEBOOK_PUBLIC', '401522313351953')
 GITHUB_PUBLIC = os.environ.get('GITHUB_PUBLIC', 'b24753ec88ca98150354')
+SOCIAL_AUTH_GITHUB_KEY = GITHUB_PUBLIC
+SOCIAL_AUTH_GITHUB_SECRET = GITHUB_SECRET
